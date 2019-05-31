2c7f6a07b8b02994d329f1ff4747c58e9103ab44
Jake Preston <jakepreston@Jakes-MacBook-Pro.local>
2019-05-31 09:37:50 -0500

    form update working

--- src/components/newsfeed/updateNewsfeedForm.js
+++ src/components/newsfeed/updateNewsfeedForm.js
@@ -1,42 +1,47 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import API from '../dbCalls/dbCalls';
 
 export default class ArticleUpdateForm extends React.Component {
-    state = {
-        userId: "",
-        type: "",
-        title: "",
-        description: "",
-        url: "",
-        eventDate: "",
-        postDate: ""
-    }
+
     handleFieldChange = evt => {
         const stateToChange = {};
         stateToChange[evt.target.id] = evt.target.value;
         this.setState(stateToChange);
     };
-    render() {
+    updateForm = () => {
+        const newsfeed = {
+            userId: parseInt(this.props.currentUserId),
+            type: "article",
+            title: this.state.title,
+            description: this.state.description,
+            url: this.state.url,
+            eventDate: this.state.eventDate,
+            postDate: Date.now()
+        }
         console.log(this.props.newsItem)
+        API.patch("newsfeed", this.props.newsItem.id, newsfeed)
+    }
+    render() {
         return (
             < Form >
                 <FormGroup>
                     <Label for="Title">Title</Label>
-                    <Input type="text" name="title" id="title" placeholder="Title" onChange={this.handleFieldChange} value={this.props.newsItem.title} />
+                    <Input type="text" name="title" id="title" placeholder="Title" onChange={this.handleFieldChange} defaultValue={this.props.newsItem.title} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="">Date Published</Label>
-                    <Input type="date" name="articleDate" id="eventDate" onChange={this.handleFieldChange} value={this.props.newsItem.eventDate} />
+                    <Input type="date" name="articleDate" id="eventDate" onChange={this.handleFieldChange} defaultValue={this.props.newsTitle} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="url">Link</Label>
-                    <Input type="url" name="url" id="url" placeholder="Url" onChange={this.handleFieldChange} value={this.props.newsItem.url} />
+                    <Input type="url" name="url" id="url" placeholder="Url" onChange={this.handleFieldChange} defaultValue={this.props.newsItem.url} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="synopsis">Synopsis</Label>
-                    <Input type="textarea" name="text" id="description" onChange={this.handleFieldChange} value={this.props.newsItem.description} />
+                    <Input type="textarea" name="text" id="description" onChange={this.handleFieldChange} defaultValue={this.props.newsItem.description} />
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button onClick={() => { this.updateForm(); this.props.toggle() }}>Submit</Button>
             </Form >
         );
     }
