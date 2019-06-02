927cfa2841ef965f394a2b61a41d24eae5c1c849
Carly Bergthold <carlybergthold@gmail.com>
2019-06-02 15:24:41 -0500

    local

--- src/components/newsfeed/NewsFeedCard.js
+++ src/components/newsfeed/NewsFeedCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardLink,
+import { Card, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle, Button, CardHeader } from 'reactstrap';
 import { Route, Redirect, withRouter } from "react-router-dom"
 
